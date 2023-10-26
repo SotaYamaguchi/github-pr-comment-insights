7cc905af054fd8f2a8358f9ddeae4e8c079f6bd5
SotaYamaguchi <sota.yamaguchi@roxx.co.jp>
2023-10-27 00:47:04 +0900

    feat: 過去のレビューコメント取得し、良い感じにみやすいようにした

--- index.ts
+++ index.ts
@@ -4,12 +4,16 @@ import axios from "axios";
 const OWNER = 'SotaYamaguchi';
 const REPO = 'github-pr-comment-insights';
 const TOKEN = '';
-const SINCE_DATE = new Date(Date.now() - 10 * 24 * 60 * 60 * 1000).toISOString();
+const ONE_YEAR_AGO = new Date(Date.now() - 364 * 24 * 60 * 60 * 1000);
 
 const QUERY = `
-  query GetRecentPRs($owner: String!, $repo: String!) {
+  query GetRecentPRs($owner: String!, $repo: String!, $after: String) {
     repository(owner: $owner, name: $repo) {
-      pullRequests(first: 100, states: [OPEN, CLOSED, MERGED], orderBy: {field: CREATED_AT, direction: DESC}) {
+      pullRequests(first: 10, states: [OPEN, CLOSED, MERGED], orderBy: {field: CREATED_AT, direction: DESC}, after: $after) {
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
         edges {
           node {
             title
@@ -19,12 +23,13 @@ const QUERY = `
               edges {
                 node {
                   body
-                  createdAt
                   comments(first: 10) {
                     edges {
                       node {
                         body
                         createdAt
+                        position
+                        path
                       }
                     }
                   }
@@ -46,66 +51,150 @@ const QUERY = `
   }
 `;
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function fetchReviewComments() {
-    try {
-        const response = await axios.post('https://api.github.com/graphql', {
-            query: QUERY,
-            variables: {
-                owner: OWNER,
-                repo: REPO
+    let after = null;
+    let allPRs = [];
+
+    while (true) {
+        console.log('fetching start')
+        try {
+            const response = await axios.post('https://api.github.com/graphql', {
+                query: QUERY,
+                variables: {
+                    after: after,
+                    owner: OWNER,
+                    repo: REPO
+                }
+            }, {
+                headers: {
+                    'Authorization': `Bearer ${TOKEN}`
+                },
+                timeout: 10000
+            });
+            console.log(response.data);
+            console.log(response.data.data.repository.pullRequests.pageInfo)
+
+            const prEdges = response.data.data.repository.pullRequests.edges;
+            allPRs = allPRs.concat(prEdges);
+
+            const lastPrDate = new Date(prEdges[prEdges.length - 1].node.createdAt);
+
+            if (lastPrDate <= ONE_YEAR_AGO) {
+                console.log('lastPrDate <= ONE_YEAR_AGO')
+                break;
             }
-        }, {
-            headers: {
-                'Authorization': `Bearer ${TOKEN}`
+
+            if (!response.data.data.repository.pullRequests.pageInfo.hasNextPage) {
+                console.log('hasNextPage is false')
+                break;
+            }
+
+            after = response.data.data.repository.pullRequests.pageInfo.endCursor;
+            console.log(after);
+            await sleep(2000);
+
+        } catch (error) {
+            console.error("Error fetching PRs:", error);
+            break;
+        }
+        console.log('fetching end')
+    }
+
+    return allPRs;
+}
+
+class ReviewComments {
+    private readonly pullRequests: any[];
+
+    constructor(pullRequests: any[]) {
+        this.pullRequests = this.excludeDependencyUpdates(pullRequests);
+    }
+
+    public getFormattedReviewComments(): any[] {
+        const relevantPullRequests = this.filterRelevantPullRequests(this.pullRequests);
+
+        return relevantPullRequests.map(pr => ({
+            title: pr.title,
+            url: pr.url,
+            createdAt: pr.createdAt,
+            comments: [...pr.reviews, ...pr.comments]
+        }));
+    }
+
+    private excludeDependencyUpdates(pullRequests: any[]): any[] {
+        return pullRequests.filter(pr => !pr.node.title.includes('chore(deps):'));
+    }
+
+    private filterRelevantPullRequests(pullRequests: any[]): any[] {
+        return pullRequests
+            .map(pr => this.reviewConversations(pr.node))
+            .filter(pr => pr.reviews.length || pr.comments.length);
+    }
+
+    private reviewConversations(node: any) {
+        return {
+            title: node.title,
+            url: node.url,
+            createdAt: node.createdAt,
+            reviews: this.getReviews(node.reviews.edges),
+            comments: this.getComments(node.comments.edges)
+        };
+    }
+
+    private getReviews(edges: any[]): any[] {
+        const reviewsWithPosition: { [position: number]: string[] } = {};
+        const nullReviews: string[][] = [];
+
+        edges.forEach(edge => {
+            if (edge.node.comments.edges.length) {
+                edge.node.comments.edges.forEach(commentEdge => {
+                    const { body, position } = commentEdge.node;
+
+                    if (position === null) {
+                        nullReviews.push([body]);
+                    } else {
+                        if (!reviewsWithPosition[position]) {
+                            reviewsWithPosition[position] = [];
+                        }
+                        reviewsWithPosition[position].push(body);
+                    }
+                });
             }
         });
 
-        return response.data.data.repository.pullRequests.edges;
-    } catch (error) {
-        console.error("Error fetching PRs:", error);
-    }
-}
-
-// @ts-ignore
-function processComments(edges, csvData, pr) {
-    for (const edge of edges) {
-        const comment = edge.node;
-        if (new Date(comment.createdAt) >= new Date(SINCE_DATE) && comment.body.trim()) {
-            csvData.push([pr.url, pr.title, comment.body, comment.createdAt]);
+        const positionedReviews = [];
+        for (const key in reviewsWithPosition) {
+            positionedReviews.push(reviewsWithPosition[key]);
         }
-    }
-}
 
-// @ts-ignore
-function processReviewComments(review, csvData, pr) {
-    if (new Date(review.createdAt) >= new Date(SINCE_DATE) && review.body.trim()) {
-        csvData.push([pr.url, pr.title, review.body, review.createdAt]);
+        return [...positionedReviews, ...nullReviews];
     }
 
-    // Process comments associated with the review
-    for (const commentEdge of review.comments.edges) {
-        const comment = commentEdge.node;
-        if (new Date(comment.createdAt) >= new Date(SINCE_DATE) && comment.body.trim()) {
-            csvData.push([pr.url, pr.title, comment.body, comment.createdAt]);
-        }
+    private getComments(edges: any[]): any[] {
+        return edges.map(edge => [edge.node.body]);
     }
 }
 
 async function main() {
     const pullRequests = await fetchReviewComments();
-    const csvData = [['Pull Request Link', 'Pull Request Title', 'Review Comment', 'Comment Date']];
+    const reviewComments = new ReviewComments(pullRequests);
+    const formattedReviewComments = reviewComments.getFormattedReviewComments();
 
-    for (const prEdge of pullRequests) {
-        const pr = prEdge.node;
+    console.log(`Found ${formattedReviewComments.length} pull requests`);
+    const tmp_jsonString = JSON.stringify(formattedReviewComments, null, 2);
+    fs.writeFileSync('tmp_output.json', tmp_jsonString);
 
-        for (const reviewEdge of pr.reviews.edges) {
-            processReviewComments(reviewEdge.node, csvData, pr);  // Process both main review comment and associated comments
-        }
+    let filteredPullRequests = pullRequests.filter((pr: any) => {
+      return !(pr.node.reviews.edges.length === 0 && pr.node.comments.edges.length === 0)
+    })
 
-        processComments(pr.comments.edges, csvData, pr); // Process general PR comments
-    }
-
-    fs.writeFileSync('output.csv', csvData.map(row => row.join(',')).join('\n'));
+    console.log(`Found ${filteredPullRequests.length} pull requests`);
+    const jsonString = JSON.stringify(filteredPullRequests, null, 2);
+    fs.writeFileSync('output.json', jsonString);
 }
 
 main().catch(error => {
